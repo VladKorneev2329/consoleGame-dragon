c9b751f5fcfa6c5944ff6977e36b1a98d229cfaf
vlad_korneev_2329 <vladkorneev2329@gmail.com>
2020-10-21 14:40:40 +0300

    first commit

---
+++ main.py
@@ -0,0 +1,17 @@
+class Dragon:
+    """
+    Экземпляр дракона
+    """
+    def __init__(self, name):
+        """
+        Конструктор класса дракон
+        :param name: str, Имя дракона
+        """
+        pass
+
+    def say
+
+
+def main():
+    """Главный цикл"""
+    pass
