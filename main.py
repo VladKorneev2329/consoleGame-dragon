a2b702e34ca23ad0d9830b86433b2d30beabd159
vlad_korneev_2329 <vladkorneev2329@gmail.com>
2020-10-27 21:04:54 +0300

    Шаблон начала программы

--- main.py
+++ main.py
@@ -1,17 +1,10 @@
-class Dragon:
-    """
-    Экземпляр дракона
-    """
-    def __init__(self, name):
-        """
-        Конструктор класса дракон
-        :param name: str, Имя дракона
-        """
-        pass
-
-    def say
-
 
 def main():
-    """Главный цикл"""
     pass
+
+if __name__ == '__main__':
+    main()
+
+
+
+
